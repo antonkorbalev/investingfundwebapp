e0dcedb6da2d6f0bfb9ce8543c09af5a7b3979c9
Anton <anton.korbalev@gmail.com>
2018-05-24 12:28:03 +0300

    Message form.

--- InvestingApp/Controllers/MessageController.cs
+++ InvestingApp/Controllers/MessageController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InvestingApp.Models;
 
 namespace InvestingApp.Controllers
 {
     public class MessageController : Controller
     {
+
+        [HttpPost]
+        public ActionResult Index(MessageModel message)
+        {
+            
+            return View();
+        }
+
         // GET: Message
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Send a message";
+            return View(new MessageModel());
         }
     }
 }
---
+++ InvestingApp/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvestingApp.Models
+{
+    public class MessageModel
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string Phone { get; set; }
+        public string Content { get; set; }
+    }
+}
