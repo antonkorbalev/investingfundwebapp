5444a069fdf15ff160c24731100fe88bfce50af1
Anton <anton.korbalev@gmail.com>
2018-05-25 12:56:47 +0300

    Send mail via SMTP.

--- InvestingApp/Controllers/MessageController.cs
+++ InvestingApp/Controllers/MessageController.cs
@@ -8,6 +8,7 @@ using System.Collections.Specialized;
 using InvestingApp.Models;
 using System.Text;
 using System.Web.Script.Serialization;
+using System.Net.Mail;
 
 namespace InvestingApp.Controllers
 {
@@ -44,6 +45,34 @@ namespace InvestingApp.Controllers
             var success = true;
             if (!checkCaptcha() || (!checkMessage(message)))
                 success = false;
+
+            if (success)
+            {
+                var mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("web@akinvest.tech");
+                mailMessage.To.Add(System.Web.Configuration.WebConfigurationManager.AppSettings["email_to"]);
+                mailMessage.Subject = "New message from AK investing techs";
+                mailMessage.IsBodyHtml = true;
+                var bld = new StringBuilder();
+                bld.AppendFormat("<b>Name:</b> {0}", message.Name);
+                bld.AppendLine("<br>");
+                bld.AppendFormat("<b>Email:</b> {0}", message.EMail);
+                bld.AppendLine("<br>");
+                bld.AppendFormat("<b>Phone:</b> {0}", message.Phone);
+                bld.AppendLine("<br>");
+                bld.AppendLine(message.Content.Replace(Environment.NewLine, "<br>"));
+                mailMessage.Body = bld.ToString();
+
+                var client = new SmtpClient("localhost", 25);
+                try
+                {
+                    client.Send(mailMessage);
+                }
+                catch
+                {
+                    success = false;
+                }
+            }
 
             var status = new RequestStatusMessage()
             {
