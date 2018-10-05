645f16f5a03c9b6325c1546f1c9ed41265846e37
Anton Korbalev <anton.korbalev@gmail.com>
2018-10-05 23:38:02 +0300

    Mean profits diagram restylement.

--- InvestingApp/App_Start/BundleConfig.cs
+++ InvestingApp/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace InvestingApp
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                      "~/Scripts/site.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ InvestingApp/Scripts/site.js
@@ -0,0 +1,3 @@
+﻿function getProfitsToolTip(desc, profit, profitPercent) {
+    return '<div class="graph-tooltip"><div><strong>' + desc + '</strong></div>' + '<div>Profit:&nbsp;<strong>' + profit.toLocaleString() + '</strong>&nbsp;RUB</div><div>(<strong>' + profitPercent.toLocaleString() + '</strong>&nbsp;%)</div></div>';
+}
